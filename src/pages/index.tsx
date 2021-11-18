a7752e12d9f833e92c8337551227f6e9ff8085b1
unknown <chromegold70@gmail.com>
2021-11-18 11:38:21 -0300

    Add form login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+export default function SignIn() {
   return (
-    <>
-      <h1>Dashgo</h1>
-    </>
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="Email" />
+          <Input name="passaword" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
